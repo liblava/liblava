179304bc711e67152ff9e2489e61d800889862f7
Pablo Escobar <mail@rvrs.in>
2021-05-19 02:43:15 +0200

    add light sample

---
+++ res/light/gen_spirv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+glslangValidator -V -o gbuffer.fragment.spirv gbuffer.frag
+glslangValidator -V -o gbuffer.vertex.spirv gbuffer.vert
+
+glslangValidator -V -o lighting.fragment.spirv lighting.frag
+glslangValidator -V -o lighting.vertex.spirv lighting.vert
