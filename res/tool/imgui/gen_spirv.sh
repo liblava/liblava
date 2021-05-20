5fa0e469dca67adcb075e72deadcd1ff01a107a0
Pablo Escobar <mail@rvrs.in>
2021-05-20 21:57:24 +0200

    clean up demo and tools shaders
    - don't generate u32 files unless needed
    - directly include u32 file

--- res/tool/imgui/gen_spirv.sh
+++ res/tool/imgui/gen_spirv.sh
@@ -2,6 +2,3 @@
 
 glslangValidator -V -x -o imgui.frag.u32 imgui.frag
 glslangValidator -V -x -o imgui.vert.u32 imgui.vert
-
-glslangValidator -V -o fragment.spirv imgui.frag
-glslangValidator -V -o vertex.spirv imgui.vert
