1eaa278215ad9853022aa5a2b2288581db823447
Cons-Cat <wgooch2000@gmail.com>
2021-06-29 22:20:10 -0400

    Extracted generic mesh implementation

--- liblava/resource/mesh.hpp
+++ liblava/resource/mesh.hpp
@@ -8,7 +8,6 @@
 #pragma once
 
 #include <liblava/resource/buffer.hpp>
-// #include <liblava/resource/generic_mesh.hpp>
 
 namespace lava {
 
@@ -193,52 +192,6 @@ private:
     bool mapped = false;
     VmaMemoryUsage memory_usage = VMA_MEMORY_USAGE_CPU_TO_GPU;
 };
-
-//-----------------------------------------------------------------------------
-template<typename T>
-void generic_mesh<T>::bind(VkCommandBuffer cmd_buf) const {
-    if (vertex_buffer && vertex_buffer->valid()) {
-        std::array<VkDeviceSize, 1> const buffer_offsets = { 0 };
-        std::array<VkBuffer, 1> const buffers = { vertex_buffer->get() };
-
-        vkCmdBindVertexBuffers(cmd_buf, 0, to_ui32(buffers.size()), buffers.data(), buffer_offsets.data());
-    }
-
-    if (index_buffer && index_buffer->valid())
-        vkCmdBindIndexBuffer(cmd_buf, index_buffer->get(), 0, VK_INDEX_TYPE_UINT32);
-}
-
-//-----------------------------------------------------------------------------
-template<typename T>
-void generic_mesh<T>::draw(VkCommandBuffer cmd_buf) const {
-    if (!data.indices.empty())
-        vkCmdDrawIndexed(cmd_buf, to_ui32(data.indices.size()), 1, 0, 0, 0);
-    else
-        vkCmdDraw(cmd_buf, to_ui32(data.vertices.size()), 1, 0, 0);
-}
-
-//-----------------------------------------------------------------------------
-template<typename T>
-void generic_mesh<T>::destroy() {
-    vertex_buffer = nullptr;
-    index_buffer = nullptr;
-
-    device = nullptr;
-}
-
-//-----------------------------------------------------------------------------
-template<typename T>
-bool generic_mesh<T>::reload() {
-    auto dev = device;
-    destroy();
-
-    return create(dev, mapped, memory_usage);
-}
-
-template<typename T>
-inline std::shared_ptr<generic_mesh<T>> generic_make_mesh() {
-    return std::make_shared<generic_mesh<T>>();
-}
 
 /**
  * @brief Mesh
@@ -474,59 +427,6 @@ enum class mesh_type : type {
  */
 mesh::ptr create_mesh(device_ptr device, mesh_type type);
 
-//-----------------------------------------------------------------------------
-template<typename T = vertex, typename PosType>
-std::shared_ptr<generic_mesh<T>> generic_create_mesh(device_ptr device, mesh_type type);
-
-template<typename T>
-bool generic_mesh<T>::create(device_ptr d, bool m, VmaMemoryUsage mu) {
-    device = d;
-    mapped = m;
-    memory_usage = mu;
-
-    if (!data.vertices.empty()) {
-        vertex_buffer = make_buffer();
-
-        if (!vertex_buffer->create(device, data.vertices.data(), sizeof(vertex) * data.vertices.size(), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, mapped, memory_usage)) {
-            log()->error("create mesh vertex buffer");
-            return false;
-        }
-    }
-
-    if (!data.indices.empty()) {
-        index_buffer = make_buffer();
-
-        if (!index_buffer->create(device, data.indices.data(), sizeof(ui32) * data.indices.size(), VK_BUFFER_USAGE_INDEX_BUFFER_BIT, mapped, memory_usage)) {
-            log()->error("create mesh index buffer");
-            return false;
-        }
-    }
-
-    return true;
-}
-
-//-----------------------------------------------------------------------------
-template<typename T = lava::vertex, typename PosType = float>
-std::shared_ptr<generic_mesh<T>> generic_create_mesh(device_ptr device, mesh_type type, size_t pos_offset = offsetof(T, position)) {
-    std::shared_ptr<generic_mesh<T>> triangle = generic_make_mesh<T>();
-    std::array<PosType, 3> pos_one = { 1, 1, 0 };
-    T vert_one;
-    memcpy(&vert_one + pos_offset, &pos_one, sizeof(pos_one));
-    std::array<PosType, 3> pos_two = { -1, 1, 0 };
-    T vert_two;
-    memcpy(&vert_two + pos_offset, &pos_two, sizeof(pos_two));
-    std::array<PosType, 3> pos_three = { 0, -1, 0 };
-    T vert_three;
-    memcpy(&vert_three + pos_offset, &pos_three, sizeof(pos_three));
-
-    triangle->get_vertices().push_back(vert_one);
-    triangle->get_vertices().push_back(vert_two);
-    triangle->get_vertices().push_back(vert_three);
-    if (!triangle->create(device))
-        return nullptr;
-    return triangle;
-}
-
 /**
  * @brief Mesh meta
  */
@@ -542,3 +442,5 @@ struct mesh_meta {
 using mesh_registry = id_registry<mesh, mesh_meta>;
 
 } // namespace lava
+
+#include "mesh.tpp"
---
+++ liblava/resource/mesh.tpp
@@ -0,0 +1,104 @@
+#pragma once
+
+namespace lava {
+
+template<typename T>
+void generic_mesh<T>::bind(VkCommandBuffer cmd_buf) const {
+    if (vertex_buffer && vertex_buffer->valid()) {
+        std::array<VkDeviceSize, 1> const buffer_offsets = { 0 };
+        std::array<VkBuffer, 1> const buffers = { vertex_buffer->get() };
+
+        vkCmdBindVertexBuffers(cmd_buf, 0, to_ui32(buffers.size()), buffers.data(), buffer_offsets.data());
+    }
+
+    if (index_buffer && index_buffer->valid())
+        vkCmdBindIndexBuffer(cmd_buf, index_buffer->get(), 0, VK_INDEX_TYPE_UINT32);
+}
+
+//-----------------------------------------------------------------------------
+template<typename T>
+void generic_mesh<T>::draw(VkCommandBuffer cmd_buf) const {
+    if (!data.indices.empty())
+        vkCmdDrawIndexed(cmd_buf, to_ui32(data.indices.size()), 1, 0, 0, 0);
+    else
+        vkCmdDraw(cmd_buf, to_ui32(data.vertices.size()), 1, 0, 0);
+}
+
+//-----------------------------------------------------------------------------
+template<typename T>
+void generic_mesh<T>::destroy() {
+    vertex_buffer = nullptr;
+    index_buffer = nullptr;
+
+    device = nullptr;
+}
+
+
+//-----------------------------------------------------------------------------
+template<typename T>
+bool generic_mesh<T>::reload() {
+    auto dev = device;
+    destroy();
+
+    return create(dev, mapped, memory_usage);
+}
+
+template<typename T>
+inline std::shared_ptr<generic_mesh<T>> generic_make_mesh() {
+    return std::make_shared<generic_mesh<T>>();
+}
+
+//-----------------------------------------------------------------------------
+template<typename T = vertex, typename PosType>
+std::shared_ptr<generic_mesh<T>> generic_create_mesh(device_ptr device, mesh_type type);
+
+template<typename T>
+bool generic_mesh<T>::create(device_ptr d, bool m, VmaMemoryUsage mu) {
+    device = d;
+    mapped = m;
+    memory_usage = mu;
+
+    if (!data.vertices.empty()) {
+        vertex_buffer = make_buffer();
+
+        if (!vertex_buffer->create(device, data.vertices.data(), sizeof(vertex) * data.vertices.size(), VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, mapped, memory_usage)) {
+            log()->error("create mesh vertex buffer");
+            return false;
+        }
+    }
+
+    if (!data.indices.empty()) {
+        index_buffer = make_buffer();
+
+        if (!index_buffer->create(device, data.indices.data(), sizeof(ui32) * data.indices.size(), VK_BUFFER_USAGE_INDEX_BUFFER_BIT, mapped, memory_usage)) {
+            log()->error("create mesh index buffer");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+//-----------------------------------------------------------------------------
+template<typename T = lava::vertex, typename PosType = float>
+std::shared_ptr<generic_mesh<T>> generic_create_mesh(device_ptr device, mesh_type type, size_t pos_offset = offsetof(T, position)) {
+    std::shared_ptr<generic_mesh<T>> triangle = generic_make_mesh<T>();
+    std::array<PosType, 3> pos_one = { 1, 1, 0 };
+    T vert_one;
+    memcpy(&vert_one + pos_offset, &pos_one, sizeof(pos_one));
+    std::array<PosType, 3> pos_two = { -1, 1, 0 };
+    T vert_two;
+    memcpy(&vert_two + pos_offset, &pos_two, sizeof(pos_two));
+    std::array<PosType, 3> pos_three = { 0, -1, 0 };
+    T vert_three;
+    memcpy(&vert_three + pos_offset, &pos_three, sizeof(pos_three));
+
+    triangle->get_vertices().push_back(vert_one);
+    triangle->get_vertices().push_back(vert_two);
+    triangle->get_vertices().push_back(vert_three);
+    if (!triangle->create(device))
+        return nullptr;
+    return triangle;
+}
+
+}
