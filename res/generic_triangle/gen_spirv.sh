bdc1508dacf4385955dedb565fdeb134448df9c6
Lava Block <the@lava-block.com>
2021-07-05 03:55:03 +0200

    gen_spirv generic_triangle

---
+++ res/generic_triangle/gen_spirv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+glslangValidator -V -o triangle_frag.spirv triangle.frag
+glslangValidator -V -o lava_triangle.spirv lava_triangle.vert
+glslangValidator -V -o int_triangle.spirv int_triangle.vert
+glslangValidator -V -o double_triangle.spirv double_triangle.vert
