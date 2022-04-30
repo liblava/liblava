a12ef2b53040edc9ab54ca6af034f770f338e03c
Lava Block <the@lava-block.com>
2022-04-30 18:39:22 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,7 +3,7 @@ set(argh_GITHUB adishavit/argh)
 set(argh_TAG d17062c870b5919f6d1eae7fe12879869a893b32)
 
 set(glfw_GITHUB glfw/glfw)
-set(glfw_TAG 4cb36872a5fe448c205d0b46f0e8c8b57530cfe0)
+set(glfw_TAG 7dfd84c458dfa12b351c932b8281b13faf7b50b9)
 
 set(gli_GITHUB g-truc/gli)
 set(gli_TAG 779b99ac6656e4d30c3b24e96e0136a59649a869)
@@ -15,7 +15,7 @@ set(physfs_GITHUB icculus/physfs)
 set(physfs_TAG 14691399cd55e2e6408b2b2d07084881885a30bb)
 
 set(json_GITHUB nlohmann/json)
-set(json_TAG a6ee8bf9d94ef783f21f955d4125d5f3924d2c8e)
+set(json_TAG 5352856f04730204123285ed928fc720ee8348b3)
 
 set(spdlog_GITHUB gabime/spdlog)
 set(spdlog_TAG 173d06578f4cfecc94de1746b101240c93f8b45b)
@@ -42,7 +42,7 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG aa9da698177727051eef093d461a669a43c9411e)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG e54db4ee708b99e24348f31847e8032921913989)
+set(imgui_TAG 709bc03d6f07467fe10f892f9198913f0298b58e)
 
 set(imgui_docking_GITHUB ocornut/imgui)
 set(imgui_docking_TAG ec48681455694c85982861dfa3f304fd544d3137)
@@ -51,7 +51,7 @@ set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 46b7918218d02b678b9405da538928a0f2c286bb)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 671f6e633f4233a6e02e66a0b4d54ee1e67fa045)
+set(SPIRV-Tools_TAG 46492aa45ae5da92eb1ec8a8051d6a4a98df70af)
 
 set(glslang_GITHUB KhronosGroup/glslang)
 set(glslang_TAG e3bca2add61a83cc87b0db199c1fbd45690dbf0b)
@@ -60,4 +60,4 @@ set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG 96b1dd72a827304817470274a470c4d3b2293451)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG 9dc229693d7035b2a09057d578a69d97afc8e572)
+set(Catch2_TAG 332de39cd4ba410e9dd9de670ea87a7c0dc7a8dd)
