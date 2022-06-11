aecdbe9a4f7de80c841408fce5689729866a4048
Lava Block <the@lava-block.com>
2022-06-11 03:06:47 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,7 +3,7 @@ set(argh_GITHUB adishavit/argh)
 set(argh_TAG 39b9a00046143f6b300dcf98ea989486cf62097d)
 
 set(glfw_GITHUB glfw/glfw)
-set(glfw_TAG 62e175ef9fae75335575964c845a302447c012c7)
+set(glfw_TAG d3ede7b6847b66cf30b067214b2b4b126d4c729b)
 
 set(gli_GITHUB g-truc/gli)
 set(gli_TAG 779b99ac6656e4d30c3b24e96e0136a59649a869)
@@ -15,7 +15,7 @@ set(physfs_GITHUB icculus/physfs)
 set(physfs_TAG 6925c1067de2c9e39d626bcba84db0113f8395f2)
 
 set(json_GITHUB nlohmann/json)
-set(json_TAG b6d00d18974358ab1d55dcc3379d0ee3052deb4e)
+set(json_TAG 48a102c2c5ef7c2552ea291f5527a8481c98a7a7)
 
 set(spdlog_GITHUB gabime/spdlog)
 set(spdlog_TAG 298a200f69d66114adde2d5d8ad34f2cce5a5b69)
@@ -27,31 +27,31 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG 8322e00ae685ea623ab6ac5a6cebcfa2d22fbf93)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 3ef4c97fd6ea001d75a8e9da408ee473c180e456)
+set(Vulkan-Headers_TAG ec9b96aae53e152f6812e66fa43757c1256369a0)
 
 set(Vulkan-Profiles_GITHUB KhronosGroup/Vulkan-Profiles)
-set(Vulkan-Profiles_TAG 90626c3cd07d01ffc43772f5105a981718f59ec3)
+set(Vulkan-Profiles_TAG 9dd8386f81bbf7fed87c0161c8eafcabd6d2e96e)
 
 set(volk_GITHUB zeux/volk)
 set(volk_TAG ec5c199f3e2a83a2326a1b5aaebb516ee2c6082c)
 
 set(VulkanMemoryAllocator_GITHUB GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
-set(VulkanMemoryAllocator_TAG a6bfc237255a6bac1513f7c1ebde6d8aed6b5191)
+set(VulkanMemoryAllocator_TAG cfdc0f8775ab3258a3b9c4e47d8ce4b6f52a5441)
 
 set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 7d6ff1f4ba51e7a2b142be39457768abece1549c)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 0b1bcfcc205077d31493ec851f87ca4b54b4eab5)
+set(imgui_TAG 076d8fc86889b482785902628f64216533cdf849)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 4789c7e485244aa6489f89dbb03b19d4ad0ea1ec)
+set(imgui_docking_TAG e900ca355e3c7be7672e25800af346f1179e91d8)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 36c0c1596225e728bd49abb7ef56a3953e7ed468)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG fad68a75519fe8c2dddbb9b3b845fd1bb0e07d4c)
+set(SPIRV-Tools_TAG 8f7f5024f86c74a5a007e456b04346d48be486ed)
 
 set(glslang_GITHUB KhronosGroup/glslang)
 set(glslang_TAG adbf0d3106b26daa237b10b9bf72b1af7c31092d)
