780305a62b466790fba460f3c8e44e76324174aa
Lava Block <the@lava-block.com>
2022-07-14 22:00:05 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,7 +3,7 @@ set(argh_GITHUB adishavit/argh)
 set(argh_TAG 39b9a00046143f6b300dcf98ea989486cf62097d)
 
 set(glfw_GITHUB glfw/glfw)
-set(glfw_TAG 36f0bf00a92b15e301bb28e840a4361e0b74b123)
+set(glfw_TAG c50d53160fa9b579dda0d0a4f9a7c2512940df8e)
 
 set(gli_GITHUB g-truc/gli)
 set(gli_TAG 779b99ac6656e4d30c3b24e96e0136a59649a869)
@@ -27,10 +27,10 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG a1e8bad32e1ccd26a7936c5354ecf856aec2cf59)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 9f73b931f402f23554a60015924e7e35c7047487)
+set(Vulkan-Headers_TAG 16847a61009f23b73b6de658a64e42926efc1ea9)
 
 set(Vulkan-Profiles_GITHUB KhronosGroup/Vulkan-Profiles)
-set(Vulkan-Profiles_TAG 5c2a6629efc35d2019d461ceac63ce8cb2eede2e)
+set(Vulkan-Profiles_TAG cc2d9dfca6038c2f57f36e889f5b9e188abf306a)
 
 set(volk_GITHUB zeux/volk)
 set(volk_TAG 03ddd4be77a3cf8f19809c83e4580a67fdabdc66)
@@ -42,19 +42,19 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 7d6ff1f4ba51e7a2b142be39457768abece1549c)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 1480bc5d4e00f8633f89631da4c8df3ed4acce61)
+set(imgui_TAG 8731cc2914a4c08f555bf70d93b54658d5d942ce)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 21fc57f2cfb61fc61527d7ed540a01c11e258939)
+set(imgui_docking_TAG 77637fd9363ac7ef7b6fb6419b760602192a38bd)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 0bcc624926a25a2a273d07877fd25a6ff5ba1cfb)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 69e1deabc1cdab0ec8e19cae8250236c0d22e689)
+set(SPIRV-Tools_TAG e2cf7693026bb53f5590193f8c2853d1b34d74d5)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 503dd243782ce6793c04eca551feef1985e3dcf1)
+set(glslang_TAG 68c1880c09c36843dcf4346ea87cf69f5c7dbf9e)
 
 set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG a0a247d09229669d19b268eb8051eeda880449c1)
@@ -63,4 +63,4 @@ set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 1677374f23352716fc52183255a40c1b8e1d53eb)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG 5f9109a8dc616f6aba0dabf18dd77da41f5435bc)
+set(Catch2_TAG 3bd0c58878bb4d413a4ef9ed701ec08e407420ab)
