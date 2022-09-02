432f119ecd71f332599d79bcfc50491c73f29ebe
Lava Block <the@lava-block.com>
2022-09-02 11:26:55 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -15,7 +15,7 @@ set(physfs_GITHUB icculus/physfs)
 set(physfs_TAG 0d4e9aac4575744ddaae56b146f1be19f064f0e5)
 
 set(json_GITHUB nlohmann/json)
-set(json_TAG 8fcdbf2e771f481d988cb36847d6af6b17e30a99)
+set(json_TAG 307c053b9b250abc6e6d478a4336fd98592ae173)
 
 set(spdlog_GITHUB gabime/spdlog)
 set(spdlog_TAG f44fa31f5110331af196d0ed7f00ae1c7b8ef3cc)
@@ -27,7 +27,7 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG a1e8bad32e1ccd26a7936c5354ecf856aec2cf59)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 715673702f5b18ffb8e5832e67cf731468d32ac6)
+set(Vulkan-Headers_TAG 9f4c61a31435a7a90a314fc68aeb386c92a09c0f)
 
 set(Vulkan-Profiles_GITHUB KhronosGroup/Vulkan-Profiles)
 set(Vulkan-Profiles_TAG aa3c9e1e71134c27b40e2e3bd737d6ebba02ec2b)
@@ -42,19 +42,19 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 7d6ff1f4ba51e7a2b142be39457768abece1549c)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG e13913ed572dbd95dedf840a94db5f27a1fdf2a5)
+set(imgui_TAG 5867a43dc8ee16e4c87572fd86a4b8c12c7deae5)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 71a0701920dbc83155f718182f01132d1ec2d51e)
+set(imgui_docking_TAG bc2002ab92f0eb6411fef32abd4037f449cf85a6)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
-set(SPIRV-Headers_TAG 93754d52d6cbbfd61f4e87571079e8a28e65f8ca)
+set(SPIRV-Headers_TAG 87d5b782bec60822aa878941e6b13c0a9a954c9b)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG a98f05d02fedcf2a20fd94b4640eb700b7df198d)
+set(SPIRV-Tools_TAG 4c456f7da67c5437a6fb7d4d20d78e2a5ae2acf2)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 9e78bc8108a13d4d4ed860b2c5547092059ed83e)
+set(glslang_TAG 69ae9e7460499b488cb2d32edae623a95264db14)
 
 set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG 31bddbb37ef1e974da7bce859ab59bcfc5ad9ee7)
@@ -63,4 +63,4 @@ set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG dc001fa935d71b4b77f263fce405c9dbdfcbfe28)
+set(Catch2_TAG 7b2e7d623b626ed2cae1d4b7f2b5480bdabbefd8)
