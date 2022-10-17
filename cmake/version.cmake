9b4d2744b4f96e162fe911801b86c536ee99f1df
Lava Block <the@lava-block.com>
2022-10-17 11:32:36 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -18,7 +18,7 @@ set(json_GITHUB nlohmann/json)
 set(json_TAG a3e6e26dc83a726b292f5be0492fcc408663ce55)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG 936697e5b1944bf1630cfbb9e82144f8bdc32815)
+set(spdlog_TAG d011332616464bd0c84817d6255bf5910b7803e9)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG 8b5f1f37b5b75829fc72d38e7b5d4bcbf8a26d55)
@@ -27,10 +27,10 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG 764cf751847b3e569d92ac400de7ec8c62be9ecd)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 0df9899602351e03357d3f1895cdfb73acb5d294)
+set(Vulkan-Headers_TAG 98f440ce6868c94f5ec6e198cc1adda4760e8849)
 
 set(volk_GITHUB zeux/volk)
-set(volk_TAG d0e41bf57439420253d9a9b5c3101226e68e64dd)
+set(volk_TAG f29df7d2834c434b39169d5b2e4dde8c05a5adc1)
 
 set(VulkanMemoryAllocator_GITHUB GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
 set(VulkanMemoryAllocator_TAG c351692490513cdb0e5a2c925aaf7ea4a9b672f4)
@@ -39,25 +39,25 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 685673dea6fb4012bd2104bf9b8d8da802eade50)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 5196bd15e90b56ccfaabe1ce44fd352a2e05bd51)
+set(imgui_TAG 08752b372e5ebeb39adec59387590dac9d9e68f7)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG edf522b58a8f74dee3225a0d5bc71565b1b8f6e0)
+set(imgui_docking_TAG f87e891f1818d02f55028fe042a323bb27c41d66)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 85a1ed200d50660786c1a88d9166e871123cce39)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG a3350c0ff067e863dcd478bd9b23a92c028b10f8)
+set(SPIRV-Tools_TAG b53d3a6be38b032dedbc72639dfc6249b5e92697)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 89db4e1caa273a057ea46deba709c6e50001b314)
+set(glslang_TAG 5755de46b07e4374c05fb1081f65f7ae1f8cca81)
 
 set(shaderc_GITHUB google/shaderc)
-set(shaderc_TAG ca899320dd29f2ff08d4624ace9cd1a568b73645)
+set(shaderc_TAG aa4de735c581f033dbbdd33e756e29d1b52a58e1)
 
 set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG bdf30834ebe352310f31bb16382484f828ccbf0b)
+set(Catch2_TAG 95cd95e591696bee5fbf24cf5b3a1610b4d05e3b)
