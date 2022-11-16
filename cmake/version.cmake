cd2396c352cb9cf7b71e5273eef1ce9cae1d7cfc
Lava Block <the@lava-block.com>
2022-11-16 17:43:05 +0100

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -18,7 +18,7 @@ set(json_GITHUB nlohmann/json)
 set(json_TAG a3e6e26dc83a726b292f5be0492fcc408663ce55)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG d7de159455d0d9bafc999f78139eafddb3935c36)
+set(spdlog_TAG 4f800773393d3ebac13c1fcd946a315d4d72bcd9)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG 8b5f1f37b5b75829fc72d38e7b5d4bcbf8a26d55)
@@ -39,16 +39,16 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 685673dea6fb4012bd2104bf9b8d8da802eade50)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 03d3343e215f78945d149621bd5655a95db59bfc)
+set(imgui_TAG 83cee9e091c38b9e5ce37eb09540319581d417ec)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG d7aca168eee54e1423e362384a3cfeec92b22563)
+set(imgui_docking_TAG 94e850fd6ff9eceb98fda3147e3ffd4781ad2dc7)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 47f2465ee3e78ec5ec38f00b2c405d9475797228)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 68e8327f2932339422eb6a1043ff395e9e602402)
+set(SPIRV-Tools_TAG 8ea3ae6be2b2409e6c44eba3c17006e2fa0845f2)
 
 set(glslang_GITHUB KhronosGroup/glslang)
 set(glslang_TAG 2b2523fb951f63f072cfba514c26f2feea5f4329)
@@ -60,4 +60,4 @@ set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG b65c0e27e9f42c9e96714b8c4973417589368787)
+set(Catch2_TAG 8beb74da8abd69c9cb083772edc230ea857b1229)
