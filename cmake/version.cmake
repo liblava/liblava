3b5cf28f9994a5def7cf5d333d0facdf751c0499
Lava Block <the@lava-block.com>
2022-11-17 19:06:32 +0100

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -24,10 +24,10 @@ set(stb_GITHUB nothings/stb)
 set(stb_TAG 8b5f1f37b5b75829fc72d38e7b5d4bcbf8a26d55)
 
 set(tinyobjloader_GITHUB syoyo/tinyobjloader)
-set(tinyobjloader_TAG 4a02d274ae1836b40adaa603fbafa97a76c2abc7)
+set(tinyobjloader_TAG 0a67aaa9d94bdeb96b44eb54dfff6c51197e7df9)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 29c0457cc167bfc9e9361a3818440e388986f5b5)
+set(Vulkan-Headers_TAG b7a86d3b2bf8fbe73fcd40df9ec62a5966e9db89)
 
 set(volk_GITHUB zeux/volk)
 set(volk_TAG 4baba47427568423b5a99aeb55af1cc9a64b8641)
@@ -39,19 +39,19 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 685673dea6fb4012bd2104bf9b8d8da802eade50)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 83cee9e091c38b9e5ce37eb09540319581d417ec)
+set(imgui_TAG 5bb287494096461f90eb5d18135f7c4809efd2f5)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 94e850fd6ff9eceb98fda3147e3ffd4781ad2dc7)
+set(imgui_docking_TAG 38cfe22b8b1e353e168811f8daad5dc7b8730fd0)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
-set(SPIRV-Headers_TAG 47f2465ee3e78ec5ec38f00b2c405d9475797228)
+set(SPIRV-Headers_TAG c214f6f2d1a7253bb0e9f195c2dc5b0659dc99ef)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 8ea3ae6be2b2409e6c44eba3c17006e2fa0845f2)
+set(SPIRV-Tools_TAG 1a7f71afb42983b8b21bef656260964eb3852942)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 2b2523fb951f63f072cfba514c26f2feea5f4329)
+set(glslang_TAG 88fd417b0bb7d91755961c70e846d274c182f2b0)
 
 set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG dc9d28ae95594edaef725012acd9de15a7342048)
@@ -60,4 +60,4 @@ set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG 8beb74da8abd69c9cb083772edc230ea857b1229)
+set(Catch2_TAG 65cc7fd2ae39a7a543767f661b096d7d521ee4f0)
