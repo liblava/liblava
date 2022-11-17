c679a886f890a3d167b3cacd3d62e9c7397430a5
Lava Block <the@lava-block.com>
2022-11-17 23:18:50 +0100

    minor

--- liblava/engine/producer.hpp
+++ liblava/engine/producer.hpp
@@ -12,13 +12,13 @@
 
 namespace lava {
 
-/// Shader folder
+/// shader folder
 constexpr name _shader_path_ = "shader/";
 
-/// Temp folder
+/// temp folder
 constexpr name _temp_path_ = "temp/";
 
-/// Hash file
+/// hash file
 constexpr name _hash_json_ = "hash.json";
 
 /**
