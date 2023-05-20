523f6d0ca24246655f71ffbab6280e0f28aad822
Lava Block <the@lava-block.com>
2023-05-20 19:04:44 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -18,7 +18,7 @@ set(json_GITHUB nlohmann/json)
 set(json_TAG 6af826d0bdb55e4b69e3ad817576745335f243ca)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG 57a9fd0841f00e92b478a07fef62636d7be612a8)
+set(spdlog_TAG dd173bc5442a20b3c8429a6d848e770ef96b952f)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG 5736b15f7ea0ffb08dd38af21067c314d6a3aae9)
@@ -27,7 +27,7 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG bca2719a11e688b85ce9af21dcb156f3d8b918bc)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG bae9700cd9425541a0f6029957f005e5ad3ef660)
+set(Vulkan-Headers_TAG 9e61870ecbd32514113b467e0a0c46f60ed222c7)
 
 set(volk_GITHUB zeux/volk)
 set(volk_TAG b3bc21e584f97400b6884cb2a541a56c6a5ddba3)
@@ -39,19 +39,19 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 321a095aa2704fa8b8c3a2b366eeca247a9825a5)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG bcfc1ad8f63997751a7269788511157ed872da2c)
+set(imgui_TAG df8667b18b5281205e15fa0254dce9f3e87dd6b2)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 530134d1253c97bd33268b5932b6c67d0ad47d09)
+set(imgui_docking_TAG e590b5114e5cbe7f4e858548ddf9959827a366af)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
-set(SPIRV-Headers_TAG 268a061764ee69f09a477a695bf6a11ffe311b8d)
+set(SPIRV-Headers_TAG bdbfd019be6952fd8fa9bd5606a8798a7530c853)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 51892874ba08f3ac0d9b1fcf3893c8516693a88e)
+set(SPIRV-Tools_TAG e7c6084fd1d6d6f5ac393e842728d8be309688ca)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG d9c3c7538b2449e39cfb0d793702d218ed58ff03)
+set(glslang_TAG 9caca7a17ba4637c77831e147cdafec6d5e7a403)
 
 set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG f9eb1c730c20c5fa7aab81cf7074c22c6342274f)
@@ -60,4 +60,4 @@ set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG 5a40b2275caa05cf809bf04df848764a9d7df2e2)
+set(Catch2_TAG 06c0e1cfab2a2fd6d01527b92422df2a8c49a8d7)
