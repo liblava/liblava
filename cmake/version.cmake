dc1d7d822619d15496059fe5f1f140c66ab05fb4
Lava Block <the@lava-block.com>
2023-07-03 09:48:00 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -18,7 +18,7 @@ set(json_GITHUB nlohmann/json)
 set(json_TAG 5d2754306d67d1e654a1a34e1d2e74439a9d53b3)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG da1e671d4285fce4b2b1603be90afc652c4f4b65)
+set(spdlog_TAG c3fa8f60e2aa6fd6257016436794c024c360cec8)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG 5736b15f7ea0ffb08dd38af21067c314d6a3aae9)
@@ -27,10 +27,10 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG 22441b15f129f0dcda8a472ce615ad7fdfd6f714)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG b6a29e5ca865f48368f6b2f170adb89975bb0be1)
+set(Vulkan-Headers_TAG ad5f8ee9750e99c5397d44c075ae5d8a38271de4)
 
 set(volk_GITHUB zeux/volk)
-set(volk_TAG 6a585a41e8ec760f395d106e88423dace15cc206)
+set(volk_TAG bc9bee2fc682b99a12800f56681fa65b8cb52626)
 
 set(VulkanMemoryAllocator_GITHUB GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
 set(VulkanMemoryAllocator_TAG 0aa3989b8f382f185fdf646cc83a1d16fa31d6ab)
@@ -39,19 +39,19 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 90da8021ec7c7792c454c3f43516595754a91765)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 6b2e03c5b1bc4f99dbd86d10e5fd12af9e3fe1c2)
+set(imgui_TAG 655aae591157c6f1d63a06d7565fe3e64390771c)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 6b011140576d5b7f94a6716235fc287ffb34ae34)
+set(imgui_docking_TAG a88e5be7f478233e74c72c72eabb1d5f1cb69bb5)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 3469b164e25cee24435029a569933cb42578db5d)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 310a67020a7d67be4fdf1b4bfa9bb85f985c6fd7)
+set(SPIRV-Tools_TAG 58459c2b1adb43c9e8705823b78a12e95e8c430f)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 9575e33186c74a68831c469f7271edf386ea43a5)
+set(glslang_TAG 3ebb72cc7429f0ab8218104dc3687c659c0f364d)
 
 set(shaderc_GITHUB google/shaderc)
 set(shaderc_TAG e31c4c2e41544d63d90be28c46e4a4793a624240)
