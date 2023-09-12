0a98b22e841ad4d49620a8da5281dd1be9359d85
Lava Block <the@lava-block.com>
2023-09-12 14:00:43 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -15,10 +15,10 @@ set(physfs_GITHUB icculus/physfs)
 set(physfs_TAG 7931b9b3f4ca3b0685fe35f105a3e5c2a1e0d1ed)
 
 set(json_GITHUB nlohmann/json)
-set(json_TAG 836b7beca4b62e2a99465edef44066b7401fd704)
+set(json_TAG da92c0e3e72d6a42106b5d829f5ff2d57fb16f77)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG 3aceda041b2bfbccaf14d5c83eda16fc67bea364)
+set(spdlog_TAG b5b5043d42b5d25ccf146b8ab422f72571f58301)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG 5736b15f7ea0ffb08dd38af21067c314d6a3aae9)
@@ -27,19 +27,19 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG ee45fb41db95bf9563f2a41bc63adfa18475c2ee)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG fff6d143cc4dccb5959db63c85c7b01406e0d333)
+set(Vulkan-Headers_TAG 4f51aac14f65629dfe83702b806f740dbd7bd701)
 
 set(volk_GITHUB zeux/volk)
 set(volk_TAG 623a5504a9eb9314d9c18ba36d64ef3208c215a1)
 
 set(VulkanMemoryAllocator_GITHUB GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
-set(VulkanMemoryAllocator_TAG 5a8fd79c597576a139716d48f264441733ad6d3b)
+set(VulkanMemoryAllocator_TAG 2f382df218d7e8516dee3b3caccb819a62b571a2)
 
 set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG 1a083cca7d650c0615e32a1d39d56892a2ce8c5b)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 0962c9fb723ee1498a533f0e592d81f7e6c1ffd8)
+set(imgui_TAG 9714594c355ec85c10b6940bea2612ea398e64ee)
 
 set(imgui_docking_GITHUB ocornut/imgui)
 set(imgui_docking_TAG 72dbe45adce69bc180d7682aa2df125994e2e77f)
@@ -48,7 +48,7 @@ set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG d790ced752b5bfc06b6988baadef6eb2d16bdf96)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG 47b63a4d7da04ae8d3f50a5df560c4b879308257)
+set(SPIRV-Tools_TAG fc54e178e94060dff8c06dc6957736e713737e53)
 
 set(glslang_GITHUB KhronosGroup/glslang)
 set(glslang_TAG f1cb8608b390a7f51b4ae0d62cd415ba47a59b86)
