9140b93aba42d6c9e0e35dd88e1f40c5df6ec668
Lava Block <the@lava-block.com>
2024-06-29 18:27:01 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -27,34 +27,34 @@ set(tinyobjloader_GITHUB syoyo/tinyobjloader)
 set(tinyobjloader_TAG cab4ad7254cbf7eaaafdb73d272f99e92f166df8)
 
 set(Vulkan-Headers_GITHUB KhronosGroup/Vulkan-Headers)
-set(Vulkan-Headers_TAG 8f034f6b48fd2d30f711396a021e1dc050c8941c)
+set(Vulkan-Headers_TAG 190d2cb24e90e5bf2bec0a75604a9b3586485b6d)
 
 set(volk_GITHUB zeux/volk)
-set(volk_TAG 1ee0b6642ecb947a10f4f988930c02f54cb4b577)
+set(volk_TAG 692bef4349111d5a1e24fa0fed79eee41496a09f)
 
 set(VulkanMemoryAllocator_GITHUB GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator)
-set(VulkanMemoryAllocator_TAG 7942b798289f752dc23b0a79516fd8545febd718)
+set(VulkanMemoryAllocator_TAG 257138b8f5686ae84491a3df9f90a77d5660c3bd)
 
 set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
-set(IconFontCppHeaders_TAG 778a12759ba9100f24a02efce5f2f95da89ea8a1)
+set(IconFontCppHeaders_TAG 62d27fa93d8f1f881dac18f13881dd97af66fa74)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG b902fa4c4490ddffb713f8153d1a65e1940e58d1)
+set(imgui_TAG c47928ffc0f42bd08e5643eb1d788812cba1ac3e)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 6d948ab47ecf984239af01434f3ed03808dbf188)
+set(imgui_docking_TAG 3fec562da1d5af5f5c35e236e3e843ef0eadbcf3)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 2acb319af38d43be3ea76bfabf3998e5281d8d12)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG bc28ac7c195f59b14535edec8472d97fd32a91ad)
+set(SPIRV-Tools_TAG c91d9ec1580dce89b10b0ca6a368800e2deaf1cb)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 19efb4ec60febf569ad3cf2f2fd71bbd20ff4617)
+set(glslang_TAG 33c7e30860928bab3819c3abae8297b109a02f73)
 
 set(shaderc_GITHUB google/shaderc)
-set(shaderc_TAG d93a300d80e88ffeb07048ba4cc1f368be7b14e7)
+set(shaderc_TAG d875f303474e0051b286a11dd772817ab190e02e)
 
 set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
