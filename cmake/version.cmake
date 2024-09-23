9ea0ca26b17316a1e955eb2a6a27b5f9b4485b9f
Lava Block <the@lava-block.com>
2024-09-23 18:03:22 +0200

    update

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -12,13 +12,13 @@ set(glm_GITHUB g-truc/glm)
 set(glm_TAG 33b4a621a697a305bc3a7610d290677b96beb181)
 
 set(physfs_GITHUB icculus/physfs)
-set(physfs_TAG b1a2f1f18bce3dc0dea29314b5d7fb887d4c10ae)
+set(physfs_TAG 74c30545031ca8cdb69b2f1ec173e77d79078093)
 
 set(json_GITHUB nlohmann/json)
 set(json_TAG b36f4c477c40356a0ae1204b567cca3c2a57d201)
 
 set(spdlog_GITHUB gabime/spdlog)
-set(spdlog_TAG ffd5aa41d687ec1d9b2d6ff48ca1c816f7c7f0c5)
+set(spdlog_TAG e593f6695c6065e6b345fe2862f04a519ed484e0)
 
 set(stb_GITHUB nothings/stb)
 set(stb_TAG f75e8d1cad7d90d72ef7a4661f1b994ef78b4e31)
@@ -39,25 +39,25 @@ set(IconFontCppHeaders_GITHUB juliettef/IconFontCppHeaders)
 set(IconFontCppHeaders_TAG bf610efb2e5d4361df799d0dfb5ae3977d7bba2e)
 
 set(imgui_GITHUB ocornut/imgui)
-set(imgui_TAG 510b6adc9bb0cfea012f513e358f3df962fe91b7)
+set(imgui_TAG bc77041b57893fb3d04d4df503b483bcc3884e3a)
 
 set(imgui_docking_GITHUB ocornut/imgui)
-set(imgui_docking_TAG 44a74509af96b7ff1a65eb84fda0c690ecc81fcf)
+set(imgui_docking_TAG d7cedd648b0a6809c62d3d363efb2fe08dca00eb)
 
 set(SPIRV-Headers_GITHUB KhronosGroup/SPIRV-Headers)
 set(SPIRV-Headers_TAG 2a9b6f951c7d6b04b6c21fe1bf3f475b68b84801)
 
 set(SPIRV-Tools_GITHUB KhronosGroup/SPIRV-Tools)
-set(SPIRV-Tools_TAG a2c9c2387e97ac47ec3b1f508dd1f9f354e023cc)
+set(SPIRV-Tools_TAG 24849751c4d16d52994c097d821fdba6f385f2d1)
 
 set(glslang_GITHUB KhronosGroup/glslang)
-set(glslang_TAG 02bc074ac4ad5f6927f75712476b0cd7d9bbd3f7)
+set(glslang_TAG 3e7831ba12e2c030be2b9b279312858cb6520d52)
 
 set(shaderc_GITHUB google/shaderc)
-set(shaderc_TAG 7bab426236b2ed926ce74c93824cd57aea2c35bd)
+set(shaderc_TAG 9530b884b3fd60c04b0f36b4c8547e030306ba6c)
 
 set(PicoSHA2_GITHUB okdshin/PicoSHA2)
 set(PicoSHA2_TAG 27fcf6979298949e8a462e16d09a0351c18fcaf2)
 
 set(Catch2_GITHUB catchorg/Catch2)
-set(Catch2_TAG e200443b84acbfbfab849a032300e6544d3ee9aa)
+set(Catch2_TAG fa43b77429ba76c462b1898d6cd2f2d7a9416b14)
