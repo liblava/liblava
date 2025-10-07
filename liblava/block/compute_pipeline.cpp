7f9b97be6b189f6a266dacd7db82a29dc267af25
Lava Block <the@lava-block.com>
2025-10-07 18:57:38 +0200

    bug fix #104 adjusted due to incorrect renaming in 2eeb41e4

--- liblava/block/compute_pipeline.cpp
+++ liblava/block/compute_pipeline.cpp
@@ -23,13 +23,13 @@ bool compute_pipeline::set_shader_stage(c_data::ref data,
         return false;
     }
 
-    m_shader_stage = create_pipeline_shader_stage(m_device, data, stage);
-    if (!m_shader_stage) {
+    auto shader_stage = create_pipeline_shader_stage(m_device, data, stage);
+    if (!shader_stage) {
         logger()->error("create compute pipeline shader stage");
         return false;
     }
 
-    set(m_shader_stage);
+    set(shader_stage);
     return true;
 }
 
